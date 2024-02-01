866910ed1d2ab2dc4a895c27726d7912864d4724
Yassine Rouchad <rouchadyassine@gmail.com>
2024-02-01 16:53:48 +0100

    AND using always statement

--- and_gate.v
+++ and_gate.v
@@ -1,6 +1,8 @@
 module struct_and_gate(input a,b,
-                       output c);
+                       output reg c);
         //and(c,a,b);
-        assign c = a & b;
+        //assign c = a & b;
+        always@(*)
+            c = a & b;
 
 endmodule
